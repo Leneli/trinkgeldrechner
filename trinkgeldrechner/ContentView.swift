1bafdff0fcb3f376d09a1c0eff292aaaa2a673eb
Elena Kuklina <liafell.lansera@gmail.com>
2021-04-15 14:37:47 +0300

    Initial Commit

---
+++ trinkgeldrechner/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  trinkgeldrechner
+//
+//  Created by  Elena Kuklina on 15.04.2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ trinkgeldrechner/trinkgeldrechnerApp.swift
@@ -0,0 +1,17 @@
+//
+//  trinkgeldrechnerApp.swift
+//  trinkgeldrechner
+//
+//  Created by  Elena Kuklina on 15.04.2021.
+//
+
+import SwiftUI
+
+@main
+struct trinkgeldrechnerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
